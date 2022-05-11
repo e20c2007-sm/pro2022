76e27f121db3a7f2f2b1bdf177975edba90a0748
e20c2007-sm <86354855+e20c2007-sm@users.noreply.github.com>
2022-05-12 03:50:45 +0900

    c

---
+++ main.jsx
@@ -0,0 +1,48 @@
+// ヘッダー
+function Header(props){
+    return(
+        <header>
+            <div>
+                ここはヘッダーです。
+            </div>
+        </header>
+    );
+}
+
+// フッター
+function Footer(props){
+    return(
+        <footer>
+            <div>ここはフッターです。</div>
+        </footer>
+    );
+}
+
+// メイン（ゲーム画面）
+function Main(props){
+    return(
+        <main>
+            <div id="game-container">
+                ここはメイン（ゲーム画面）
+            </div>
+        </main>
+    );
+}
+
+class Containers extends React.Component{
+    render(){
+        return(
+            <div id="site-wrapper">
+                <Header/>
+                <Main/>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+// 描画
+ReactDOM.render(
+    <Containers/>,
+    $("#containers")[0]
+);
