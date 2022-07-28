e74c60e76bb66fab32ace8355a8b271a89e84fc4
Tetsu9ya <92501156+Tetsu9ya@users.noreply.github.com>
2022-07-28 11:45:54 +0900

    Update objects.jsx

--- Objects/objects.jsx
+++ Objects/objects.jsx
@@ -1,18 +1,19 @@
-class Objects extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class Objects extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    // Farmの関数getResouceData()を実行
-    getResrcData = ()=>{
-        return this.props.getResouceData();
-    }
-
-    render(){
-        return(
-            <div id={"obj-container"}>
-                <b>山元</b>
-            </div>
-        );
-    }
+  // Farmの関数getResouceData()を実行
+  getResrcData = () => {
+    return this.props.getResouceData();
+  };
+  render() {
+    return (
+      <div id={"obj-container"}>
+        <button id="add" onClick={() => this.props.addResource(1)}>
+          CLICK
+        </button>
+      </div>
+    );
+  }
 }
